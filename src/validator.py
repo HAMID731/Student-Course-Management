6e6fd8fa241b576ce499efc1e391f36d9e9a5dce
Abimbola <Haryishar2@gmail.com>
2025-03-07 19:45:02 +0100

    task

---
+++ src/validator.py
@@ -0,0 +1,77 @@
+import re
+
+from exceptions.exception import *
+
+
+class Validator:
+
+   @staticmethod
+   def validate_first_name(first_name) -> bool:
+       if not first_name:
+           raise NullException("First Name field is required")
+       if not first_name.strip(" "):
+           raise InvalidNameLengthException("No spaces allowed amongst the letters.")
+       if len(first_name) < 3 or not first_name.isalpha():
+           raise InvalidNameLengthException("Last Name must be at least 3 characters long and contain only letters.")
+       return True
+
+   @staticmethod
+   def validate_last_name(last_name: str) -> bool:
+       if not last_name:
+           raise NullException("Last Name field is required")
+       if not last_name.strip(" "):
+           raise InvalidNameLengthException("No spaces allowed amongst the letters.")
+       if len(last_name) < 3 or not last_name.isalpha():
+           raise InvalidNameLengthException("Last Name must be at least 3 characters long and contain only letters.")
+       return True
+
+   @staticmethod
+   def validate_email(email: str) -> bool:
+       if not email:
+           raise NullException("Email field is required")
+
+       if not isinstance(email, str):
+           raise InvalidEmailPatternException("Email must be a string")
+
+       if not email.strip(" "):
+           raise InvalidEmailPatternException("Email should not be spaced")
+
+       email_pattern = r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.(com|africa|org|ng|yahoo)$'
+
+       if email_pattern.endswith(".") or email_pattern.startswith("."):
+           raise InvalidEmailPatternException("Email should not end with a period")
+
+       if not re.match(email_pattern, email):
+           raise InvalidEmailPatternException("Invalid email address.")
+
+       return True
+
+   @staticmethod
+   def validate_password(password: str) -> bool:
+       if not password:
+           raise NullException("Password field is required")
+       if not password.strip(" "):
+           raise InvalidPasswordLengthException("Invalid.")
+       if len(password) < 5:
+           raise InvalidPasswordLengthException("Password must be at least 5 characters long and contain only letters.")
+       return True
+
+   @staticmethod
+   def validate_course_code(course_code: str) -> bool:
+       if not course_code:
+           raise NullException("Course Code field is required")
+       if len(course_code) < 3 or len(course_code) > 7 or not course_code.strip(" "):
+           raise InvalidCourseCodeException("Course Code should not be spaced.")
+       if not course_code.isalnum():
+           raise InvalidCourseCodeException("Course Code should both be alphabets and numbers.")
+       return True
+
+   @staticmethod
+   def validate_course_title(course_title: str) -> bool:
+       if not course_title:
+           raise NullException("Course Title field is required")
+       if len(course_title) < 3:
+           raise InvalidCourseTitleException("Course Title should not be spaced.")
+       return True
+
+
---
+++ tests/test_validator.py
@@ -0,0 +1,82 @@
+import unittest
+
+from exceptions.exception import InvalidNameLengthException, NullException, InvalidEmailPatternException, \
+    InvalidPasswordLengthException
+from src.validator import Validator
+
+
+class MyValidatorTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.validator = Validator()
+
+    def test_both_first_name_and_last_name_is_only_series_of_letters(self):
+        first_name = "Favour"
+        last_name = "Igwe"
+        self.assertEqual(True, self.validator.validate_first_name(first_name))
+        self.assertEqual(True, self.validator.validate_last_name(last_name))
+
+    def test_that_when_first_name_is_less_than_four_it_throws_invalid_name_length_exception(self):
+        first_name = "Fr"
+        last_name = "Igwe"
+        with self.assertRaises(InvalidNameLengthException):
+            self.validator.validate_first_name(first_name)
+
+    def test_that_when_last_name_is_less_than_four_it_throws_invalid_name_length_exception(self):
+        first_name = "Favour"
+        last_name = "Ie"
+        with self.assertRaises(InvalidNameLengthException):
+            self.validator.validate_first_name(last_name)
+
+    def test_that_user_must_input_both_first_name_and_last_name_is_only_series_of_letters(self):
+        first_name = "Favour123"
+        last_name = "Ig22we"
+        with self.assertRaises(InvalidNameLengthException):
+            self.validator.validate_first_name(first_name)
+            self.validator.validate_last_name(last_name)
+
+    def test_that_user_cannot_leave_either_first_name_and_last_name_blank(self):
+        first_name = ""
+        last_name = "Igwe"
+        with self.assertRaises(NullException):
+            self.validator.validate_first_name(first_name)
+        first_name1 = "Favour"
+        last_name1 = ""
+        with self.assertRaises(NullException):
+            self.validator.validate_last_name(last_name1)
+
+    def test_that_user_input_the_correct_email_pattern(self):
+        self.validator.validate_email("Favour123@gmail.com")
+        self.assertTrue(self.validator.validate_email("Favour123@gmail.com"))
+        self.validator.validate_email("Favour@semicolon.yahoo.com")
+        self.assertTrue(self.validator.validate_email("Favour@semicolon.yahoo.com"))
+        self.validator.validate_email("africa@yahoo.org")
+        self.assertTrue(self.validator.validate_email("africa@yahoo.org"))
+
+    def test_that_email_field_being_blank_throws_null_exception(self):
+        with self.assertRaises(NullException):
+            self.validator.validate_email("")
+
+    def test_that_ending_an_email_with_full_stop_would_throw_Invalid_email_exception(self):
+        with self.assertRaises(InvalidEmailPatternException):
+            self.validator.validate_email("semicolon.yahoo.com.")
+
+    def test_that_password_is_valid(self):
+        password = self.validator.validate_password("PASSWORD")
+        self.assertTrue(password)
+
+    def test_that_password_is_not_valid(self):
+        with self.assertRaises(InvalidPasswordLengthException):
+            password = self.validator.validate_password("ORD>")
+
+    def test_that_course_title_is_valid(self):
+        title = self.validator.validate_course_title("Introduction to English")
+        self.assertTrue(title)
+
+
+
+
+
+        
+
+
