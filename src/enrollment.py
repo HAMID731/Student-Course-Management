a85a3dfbcbd662ca6020b7c00787c49ef30e2234
Abimbola <Haryishar2@gmail.com>
2025-03-09 05:14:44 +0100

    load_files bug correction

--- src/enrollment.py
+++ src/enrollment.py
@@ -1,6 +1,11 @@
+import os
+
+from src.course import Course
+
+
 class Enrollment:
 
-    def __init__(self, course, student):
+    def __init__(self, course: Course, student):
         self.course = course
         self.student = student
 
@@ -19,3 +24,18 @@ class Enrollment:
     @student.setter
     def student(self, value):
         self.__student = value
+
+    def __repr__(self):
+        return f'{self.course} {self.student}'
+
+    @staticmethod
+    def save_enrollment(course, student):
+        if not os.path.exists('enrollment.txt'):
+            with open('enrollment.txt', 'w') as file:
+                file.write(f'{course}, {student}')
+
+    @staticmethod
+    def load_enrollment():
+        if os.path.exists('enrollment.txt'):
+            with open('enrollment.txt', 'r') as file:
+                file.readline()
