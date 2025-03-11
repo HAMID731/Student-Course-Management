3609607a331a8b20d3d3e5ea53ecbc9ddb049671
HAMID <abdulhamidabari652@gmail.com>
2025-03-11 10:31:04 +0100

    validate

--- src/User.py
+++ src/User.py
@@ -1,10 +1,48 @@
 from abc import ABC, abstractmethod
 
 class User(ABC):
-    def __init__(self, email, password):
+    def __init__(self,first_name,last_name,email, password):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
 
     @abstractmethod
-    def display_info(self):
+    def register(self, first_name, last_name, email, password):
         pass
+
+    @abstractmethod
+    def login(self, email, password):
+        pass
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self.__first_name = first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self.__last_name = last_name
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, password):
+        self.__password = password
